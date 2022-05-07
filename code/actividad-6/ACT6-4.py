6c42592885392e44cc5525dd221ecb059ec9b984
leoarayav <laraya1818@gmail.com>
2022-05-07 00:30:30 -0400

    Deleted unordered files

--- code/actividad-6/ACT6-4.py
+++
@@ -1,7 +0,0 @@
-serie = 11
-x = 1
-
-while x <= 25:
-    print(serie)
-    serie = serie + 11
-    x = x + 1
--- code/actividad-6/ACT6-5.py
+++
@@ -1,7 +0,0 @@
-multiplo = 8
-x = 1
-
-while multiplo <= 500:
-    print(multiplo)
-    multiplo = multiplo + 8
-    x = x + 1
